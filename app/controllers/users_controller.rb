4a2c9699e09a65ed0db0c8d67ed821bd4845bdbc
minimous <khanhhoai95@gmail.com>
2017-03-21 23:05:22 +0700

    Finish  user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+
   def new
+  	@user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.role = "user"
+    if @user.save
+    	flash[:success] = "Welcome to Capstone Library!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+      	:password_confirmation, :birthday, :address)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	root 'static_pages#home'
   get 'static_pages/home'
 
-  get 'users/new'
-
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
