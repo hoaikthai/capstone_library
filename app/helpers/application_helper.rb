5ce282def1eea16f4e854d839aebc226d991cd20
minimous <khanhhoai95@gmail.com>
2017-03-21 22:11:05 +0700

    Finish layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+    base_title = "Capstone Library"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+	root 'static_pages#home'
+  get 'static_pages/home'
+
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
