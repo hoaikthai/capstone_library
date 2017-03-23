3c42f031269292e50b56b7dd2af23791d4cbd00e
minimous <khanhhoai95@gmail.com>
2017-03-23 10:38:52 +0700

    Finish user edit, update, index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   	if user && user.authenticate(params[:session][:password])
       login user
       params[:session][:remember_me] == 1 ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
   	else
   		flash[:danger] = "Invalid email/password"
   		render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,10 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	def current_user? user
+		user == current_user
+	end
+
 	def forget user
 		user.forget
 		cookies.delete(:user_id)
@@ -35,6 +39,15 @@ module SessionsHelper
 		user.remember
 		cookies.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	def redirect_back_or default
+		redirect_to(session[:forwarding_url || default])
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
 	end
 	
 end
